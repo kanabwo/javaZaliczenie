78d980b73ca40fd3bb1c4df71e6e4b4ccb9bc746
kanabwo <kanabusw@gmail.com>
2021-05-22 12:03:14 +0200

    2. Stworzyć klasę abstrakcyjną Vehicle. (1pkt)
    3. Każdy pojazd ma swój numer rejestracyjny, numer vin, kolor, cenę, spalanie, stan zbiornika paliwa oraz licznik przejechanych kilometrów. (1pkt)

---
+++ src/pl/gda/wsb/Vehicle.java
@@ -0,0 +1,15 @@
+package pl.gda.wsb;
+
+public abstract class Vehicle {
+    private String regPlate;
+    private String vin;
+    private String colour;
+    private float price;
+    private float fuelEconomy;
+    private float tankLevel;
+    private float milage;
+
+
+
+
+}
